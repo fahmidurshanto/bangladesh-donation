cf12f2a7d4d6e3fb7b117ecea94f4285de06a79a
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-12-08 16:54:37 +0600

    Added donation and history button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,17 @@ function updateTime() {
   const timeString = currentDateTime.toString();
   document.getElementById("current-time").innerText = timeString;
 }
+
+
+// Donation button function
+document.getElementById('donation-btn').addEventListener("click", function (){
+    document.getElementById("donation-card-container").classList.remove("hidden");
+    document.getElementById("history-container").classList.add("hidden");
+})
+
+
+// history button function
+document.getElementById("history-btn").addEventListener('click', function(){
+    document.getElementById("history-container").classList.remove("hidden");
+    document.getElementById("donation-card-container").classList.add("hidden");
+})
